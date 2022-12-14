ccb879d544c0e751bdf1481cfe482466580b33df
milen855 <milina.mgn@gmail.com>
2022-12-14 15:05:52 -0800

    islower

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,7 +8,6 @@
 void print_alphabet(void)
 {
 	int start = 97, end =122;
-
 	while (start <= end)
 	{
 		_putchar(start);
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * islower - checks whether character passed is lowercase
+ *
+ * @c: character to be tested
+ *
+ * Return: 1 if @cc is lowercase otherwise 0
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c<= 122)
+		return (1);
+	else
+		return (0);
+}
