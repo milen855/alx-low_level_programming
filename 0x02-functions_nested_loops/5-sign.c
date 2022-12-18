39e140d5d0735d8c4f3b2092c933f97706415096
milen855 <milina.mgn@gmail.com>
2022-12-18 02:13:51 -0800

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,6 @@
 
 /**
  * print_sign - prints the sign of a number
- *
  * @n: number whose sign is to be printed
  *
  * Return: 1 if n > 0, 0 if n==0 and -1 if n < 0
@@ -15,7 +14,7 @@ int print_sign(int n)
 		return (1);
 	}
 	else if (n < 0)
-	{ 
+	{
 		_putchar('-');
 		return (-1);
 	}
