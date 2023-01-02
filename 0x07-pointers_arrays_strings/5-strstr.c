f7e338e95ea38de5728ceaa3df7ee27a42cae876
milen855 <milina.mgn@gmail.com>
2023-01-02 10:59:46 -0800

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -6,7 +6,6 @@
  * @needle: input
  * Return: Always 0 (Success)
  */
-
 char *_strstr(char *haystack, char *needle)
 {
 	for (; *haystack != '\0'; haystack++)
@@ -19,8 +18,10 @@ char *_strstr(char *haystack, char *needle)
 			l++;
 			p++;
 		}
+		
 		if (*p == '\0')
 			return (haystack);
 	}
+	
 	return (0);
 }
