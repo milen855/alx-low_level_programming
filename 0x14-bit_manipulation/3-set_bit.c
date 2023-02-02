d6a0ef1f3f9eca64e162abd6a81ee57892de73d9
milen855 <milina.mgn@gmail.com>
2023-02-02 12:37:15 -0800

    update

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -6,8 +6,9 @@
  * @index: The index to set the value at - indices start at 0.
  *
  * Return: If an error occurs - -1.
- * Otherwise - 1.
+ *         Otherwise - 1.
  */
+
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= (sizeof(unsigned long int) * 8))
